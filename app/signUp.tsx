d4b859172b7105f50df773cd382cc8e18f1d2d79
dalva8711 <dalvarezdejesus@gmail.com>
2025-08-28 10:50:13 -0400

    Added Footer To Sign Up Page

--- app/signUp.tsx
+++ app/signUp.tsx
@@ -4,18 +4,31 @@ import { wp } from '@/helpers/common'
 import { router } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 export default function SignUp() {
     return (
         <ScreenWrapper bg='white'>
             <StatusBar style='dark' />
 
-            {/* Back Button */}
+            
             <View style={styles.container}>
                 <BackButton onPress={()=>{router.back()}} />
+
+
+                {/* Footer */}
+                <View style={styles.footer}>
+                    <View style={styles.footerContainer}>
+                        <Text style={styles.footerText}>Don't have an account?</Text>
+                        <Pressable onPress={()=>{router.push('/signUp')}}>
+                            <Text style={styles.footerSignUp}>Sign Up</Text>
+                        </Pressable>
+                    </View>
+                </View>
             </View>
-            
+
+
+
         </ScreenWrapper>
     )
 }
@@ -25,5 +38,27 @@ const styles = StyleSheet.create({
         flex: 1,
         gap: 45,
         paddingHorizontal: wp(5),
+    },
+    footer: {
+        width: '100%',
+        paddingHorizontal: wp(4),
+        gap: 30,
+    },
+    footerText: {
+        fontSize: 16,
+        color: 'black',
+        textAlign: 'center',
+    }, 
+    footerSignUp: {
+        color: 'blue',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    footerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
     },
 })
